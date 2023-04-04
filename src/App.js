c06e8691eb84a86b64b180e1357742a996b2a03b
ProToTime3560 <prototime3560@naver.com>
2023-04-05 02:03:31 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ function App() {
   function 제목바꾸기() {
 
     let newArray = [...글제목];
-        //var newArray = [...글제목]; 이것도 된다.
+    //var newArray = [...글제목]; 이것도 된다.
     newArray[0] = '여자코트 추천';
     글제목변경(newArray);
   }
 
+  //https://hianna.tistory.com/409
   function 가나다순정렬() {
     let newArray = [...글제목];
-    newArray.sort(function(a,b) {
-      if(a > b) return 1;
-      if(a == b) return 0;
-      if(a < b) return -1;
+    newArray.sort(function (a, b) {
+      if (a > b) return 1;
+      if (a == b) return 0;
+      if (a < b) return -1;
     });
     글제목변경(newArray);
   }
@@ -36,7 +37,7 @@ function App() {
       < div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={()=> 가나다순정렬()} >가나다순정렬</button>
+      <button onClick={() => 가나다순정렬()} >가나다순정렬</button>
 
       <button onClick={() => { 제목바꾸기() }}>글수정</button>
       <div className="list">
@@ -54,8 +55,33 @@ function App() {
         <p>2월 19일 발행</p>
         <hr />
       </div>
+      <Modal></Modal>
+      <Modal/>
+
     </div>
   );
+}
+
+/**컴포넌트 쓰는법
+  1.반복적인 html 축약
+  2.큰페이지들
+  3. 자주변경되는 것들
+  let Modal = () => {
+    return ()
+  } 
+  이것도 됨
+  const Modal = () => {} 이렇게 만들면 나중에 에러가 생길시 알려줌
+*/
+function Modal() {
+  return (
+  <>
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  </>
+  )
 }
 
 export default App;
